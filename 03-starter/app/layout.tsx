c79199ea465a1c2504e6a1534e356ed3ba96cb7f
hbhangale3 <hbhangale3@gmail.com>
2025-08-12 03:23:31 -0700

    installed Shadcn/ui setup navbar added dark/light mode functionality

--- 03-starter/app/layout.tsx
+++ 03-starter/app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
+import Navbar from '@/components/navbar/Navbar';
+import Providers from './providers';
 
 export const metadata: Metadata = {
-  title: 'HomeAway Draft',
+  title: 'HomeAway',
   description: 'Feel at home, away from home.',
 };
 
@@ -16,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
+      <Providers>
+        <Navbar/>
         <main className='container py-10'>{children}</main>
+        </Providers>
       </body>
     </html>
   );
