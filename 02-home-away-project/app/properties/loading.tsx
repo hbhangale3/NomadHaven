490d765ab9856695d3dc26d1d5ecd34103325d54
Harshwardhan Bhangale <hbhangale3@gmail.com>
2025-08-12 03:33:18 -0700

    Delete Directory 02

--- 02-home-away-project/app/properties/loading.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { Skeleton } from '@/components/ui/skeleton';
-
-function loading() {
-  return <Skeleton className='h-[300px] md:h-[500px] w-full rounded' />;
-}
-export default loading;
--- 02-home-away-project/components/properties/Title.tsx
+++
@@ -1,4 +0,0 @@
-function Title({ text }: { text: string }) {
-  return <h3 className='text-lg font-bold mb-2'>{text}</h3>;
-}
-export default Title;
