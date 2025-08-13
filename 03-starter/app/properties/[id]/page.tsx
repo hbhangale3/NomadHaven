718229562390b67d413fefe41e8ec94647bdd712
hbhangale3 <hbhangale3@gmail.com>
2025-08-13 16:52:55 -0700

    Added property description and amenities detail to property details page

--- 03-starter/app/properties/[id]/page.tsx
+++ 03-starter/app/properties/[id]/page.tsx
@@ -6,12 +6,19 @@ import ShareButton from '@/components/properties/ShareButton';
 import ImageContainer from '@/components/properties/ImageContainer';
 import PropertyRating from '@/components/card/PropertyRating';
 import BookingCalendar from '@/components/properties/booking/BookingCalendar';
+import PropertyDetails from '@/components/properties/PropertyDetails';
+import UserInfo from '@/components/properties/UserInfo';
+import Description from '@/components/properties/Description';
+import { Separator } from '@/components/ui/separator';
+import Amenities from '@/components/properties/Amenities';
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
   if (!property) redirect('/');
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
+  const firstName = property.profile.firstName;
+  const profileImage = property.profile.profileImage;
   return (
   <section>
     <BreadCrumbs name={property.name} />
@@ -32,6 +39,11 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
         <h1 className='text-xl font-bold'>{property.name}</h1>
         <PropertyRating inPage propertyId={property.id} />
       </div>
+      <PropertyDetails details={details} />
+      <UserInfo profile={{ firstName, profileImage }} />
+      <Separator className='mt-4' />
+        <Description description={property.description} />
+        <Amenities amenities={property.amenities} />
     </div>
     <div className='lg:col-span-4 flex flex-col items-center'>
       {/* calendar */}
