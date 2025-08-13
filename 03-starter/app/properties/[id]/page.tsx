5dc740f56d96d8e01bd6e49ec10d76d03948b496
hbhangale3 <hbhangale3@gmail.com>
2025-08-13 16:25:12 -0700

    Added share button functionality

--- 03-starter/app/properties/[id]/page.tsx
+++ 03-starter/app/properties/[id]/page.tsx
@@ -2,6 +2,7 @@ import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 import BreadCrumbs from '@/components/properties/BreadCrumbs';
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import ShareButton from '@/components/properties/ShareButton';
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
@@ -14,7 +15,10 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
       <h1 className='text-4xl font-bold '>{property.tagline}</h1>
       <div className='flex items-center gap-x-4'>
         {/* share button */}
-        <FavoriteToggleButton propertyId={property.id} />
+  <div className='flex items-center gap-x-4'>
+    <ShareButton name={property.name} propertyId={property.id} />
+    <FavoriteToggleButton propertyId={property.id} />
+  </div>
       </div>
     </header>
   </section>);
