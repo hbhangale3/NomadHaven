e529af87b351c34f80e461438553aac2aecc6c75
hbhangale3 <hbhangale3@gmail.com>
2025-08-13 13:33:56 -0700

    Added helper functions to validate data and provide human readable and understandable error messages

--- 03-starter/utils/actions.ts
+++ 03-starter/utils/actions.ts
@@ -5,6 +5,7 @@ import db from './db';
 import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { validateWithZodSchema } from './schemas';
 
 const getAuthUser = async () => {
     const user = await currentUser();
@@ -31,7 +32,8 @@ export const createProfileAction = async (
     if (!user) throw new Error('Please login to create a profile');
 
     const rawData = Object.fromEntries(formData);
-    const validatedFields = profileSchema.parse(rawData);
+    const validatedFields = validateWithZodSchema(profileSchema, rawData);
+
 
     await db.profile.create({
       data: {
@@ -89,17 +91,13 @@ export const fetchProfileImage = async () => {
     try {
       const rawData = Object.fromEntries(formData);
   
-      const validatedFields = profileSchema.safeParse(rawData);
-      if (!validatedFields.success) {
-        const errors = validatedFields.error.errors.map((error) => error.message);
-        throw new Error(errors.join(','));
-      }
-  
+      const validatedFields = validateWithZodSchema(profileSchema, rawData);
+    
       await db.profile.update({
         where: {
           clerkId: user.id,
         },
-        data: validatedFields.data,
+        data: validatedFields,
       });
       revalidatePath('/profile');
       return { message: 'Profile updated successfully' };
